aca24757aa9c4d2d1c09c31093a58d3fe0a2189e
Kodos1122 <92407363+Kodos1122@users.noreply.github.com>
2024-03-20 18:17:03 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import PasswordStrengthMeter from './components/PasswordStrengthMeter';
+import './App.css'; // Assuming your styles are in App.css
+
+function App() {
+  const [password, setPassword] = useState('');
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>How Secure is Your Password?</h1>
+        <p>Check how strong is your password with our password strength checker.</p>
+      </header>
+      <div className="password-container">
+        <div className="password-input-container">
+          <input
+            type={isPasswordVisible ? 'text' : 'password'}
+            className="password-input"
+            placeholder="Password"
+            onChange={e => setPassword(e.target.value)}
+            value={password}
+          />
+          <i
+            className={`view-password-icon ${isPasswordVisible ? 'fas fa-eye-slash' : 'fas fa-eye'}`}
+            onMouseEnter={() => setIsPasswordVisible(true)}
+            onMouseLeave={() => setIsPasswordVisible(false)}
+          ></i>
+        </div>
+        <PasswordStrengthMeter password={password} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
